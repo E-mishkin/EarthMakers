145df4f64b01a61f42801442db0c7a6007bfb839
Robert Cox <rcox18@mail.greenriver.edu>
2020-05-05 11:27:51 -0400

    testing branch

--- app/js/earthquake_sim.js
+++ app/js/earthquake_sim.js
@@ -1,5 +1,5 @@
 /*
-    Authors:     Nicholas Perez(neperez23@gmail.com), Robert Cox
+    Authors:     Nicholas Perez(neperez23@gmail.com), Robert Cox *branch test*
     Date:       3/14/2020
     Version:    1.0
     Green River College Seismic Simulating Earthquakes.
